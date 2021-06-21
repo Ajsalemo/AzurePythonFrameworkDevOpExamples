c8d618123bd71e8f5d83f0e635812f9e26b776a0
Anthony Salemo <ajssalemo@gmail.com>
2021-06-21 10:08:38 -0400

    added a second route to the turbogears example

--- turbogears/tgapp.py
+++ turbogears/tgapp.py
@@ -7,6 +7,10 @@ class RootController(TGController):
     def index(self):
         return 'Hello World, from TurboGears2'
 
+    @expose()
+    def hello(self, person):
+        return 'Hello %s' % person
+
 
 config = MinimalApplicationConfigurator()
 config.update_blueprint({
