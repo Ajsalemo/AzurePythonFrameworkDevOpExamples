606715facf90ac719a9666329b6ef888021945fb
Anthony Salemo <ajssalemo@gmail.com>
2021-06-22 19:36:29 -0400

    updating server callable

--- dash/app.py
+++ dash/app.py
@@ -11,7 +11,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # set a variable to use the Flask server
 server = flask.Flask(__name__)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
